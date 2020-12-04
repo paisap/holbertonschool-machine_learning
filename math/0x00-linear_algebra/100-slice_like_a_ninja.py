59bbc0182e1b8b1f00c9469dd56892a9a76ecf47
paisap <santiagoaldana@gmail.com>
2020-12-03 22:08:20 -0500

    task 100

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Contains the np_slice function"""
+
+
+def np_slice(matrix, axes={}):
+    """slices a matrix along a specific axes"""
+
+    empty_slicer = slice(None, None, None)
+    slice_list = [empty_slicer] * len(matrix.shape)
+
+    for key, value in sorted(axes.items()):
+        slice_list[key] = slice(*value)
+    matrix = matrix[tuple(slice_list)]
+    return matrix
