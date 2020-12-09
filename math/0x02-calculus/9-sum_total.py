02430be19e8a33d2f929f7379f8ac3c2f4766de4
paisap <santiagoaldana@gmail.com>
2020-12-09 18:10:08 -0500

    task 9

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" make a sum """
+
+
+def summ(n, i):
+    """ te function recursive """
+    if i == n:
+        return i**2
+    else:
+        return i**2 + summ(n, (i + 1))
+
+
+def summation_i_squared(n):
+    """ recursivamente"""
+    if n < 1:
+        return None
+    return summ(n, 1)
