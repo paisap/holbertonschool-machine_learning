4cddc628e49c44b21330735a6c16bc955bea264c
paisap <santiagoaldana@gmail.com>
2020-12-10 19:24:09 -0500

    validaciones

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -9,6 +9,8 @@ def poly_derivative(poly):
     for i in poly:
         if type(i) != int and type(i) != float:
             return None
+    if len(poly) == 1:
+        return [0]
     aux = []
     for i in range(1, len(poly)):
         if poly[i] != 0:
