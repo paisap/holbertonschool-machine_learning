efcb6b916d2392153a223b1555c801698ed2d7d9
paisap <santiagoaldana@gmail.com>
2020-12-15 22:26:35 -0500

    pycodestyle and validations

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """ poisson distribution class"""
 
+
 class Poisson:
     """ class"""
 
     def __init__(self, data=None, lambtha=1.):
         """ constructor """
-        if data != None:
+        if data is not None:
             if type(data) != list:
                 raise TypeError("data must be a list")
             if len(data) <= 2:
@@ -21,13 +22,24 @@ class Poisson:
         """ calculate the pmf"""
         e = 2.7182818285
         factorial = 1
+        k = int(k)
+
+        if k < 0:
+            return 0
+
         for i in range(1, k + 1):
             factorial = factorial * i
+
         return ((e**-self.lambtha)*(self.lambtha**k))/factorial
 
     def cdf(self, k):
         """ calculate the pmf """
         resultado = 0
+        k = int(k)
+
+        if k < 0:
+            return 0
+
         for s in range(k + 1):
             resultado += self.pmf(s)
         return resultado
