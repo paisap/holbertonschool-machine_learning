850d2f0533f0678da5c6b58428c92684c2c5d5cd
paisap <santiagoaldana@gmail.com>
2020-12-16 02:59:02 -0500

    PDF

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -24,3 +24,21 @@ class Normal:
 
             self.mean = float(mean)
             self.stddev = float(stddev)
+
+    def z_score(self, x):
+        """ return x_score """
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """ return z value """
+        return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """ calculate the pdf """
+        e = 2.7182818285
+        π = 3.1415926536
+
+        formula_pdf = 1 / (self.stddev * (2 * π) ** 0.5) * \
+            e ** (- (x - self.mean)**2 / (2 * self.stddev**2))
+
+        return formula_pdf
