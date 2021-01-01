7c7a09a555d841ce645f5afe71a505fecb203f34
paisap <santiagoaldana@gmail.com>
2021-01-01 18:29:33 -0500

    task 3

--- supervised_learning/0x01-classification/3-neuron.py
+++ supervised_learning/0x01-classification/3-neuron.py
@@ -6,20 +6,13 @@ import numpy as np
 class Neuron:
     """ class """
 
-    def sigmoid(self, z):
-        """ sigmoid function """
-        e = 2.7182818285
-        aux = 1/(1+e**-z)
-        self.__A = aux
-        return aux
-
     def __init__(self, nx):
         """ contructor class """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError("x must be a positive integer")
-        self.__W = np.random.normal(0, 1, (1, nx))
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
 
@@ -41,8 +34,12 @@ class Neuron:
     def forward_prop(self, X):
         """ Calculates the forward propagation of the neuron """
         z = np.matmul(self.__W, X) + self.__b
-        z = self.sigmoid(z)
-        return z
+        self.__A = self.sigmoid(z)
+        return self.__A
+
+    def sigmoid(self, Z):
+        """ sigmoid function """
+        return 1 / (1 + np.exp(-Z))
 
     def cost(self, Y, A):
         """ calculate the cost of neuron """
