39adbbe87611d725151926c615f818e852b6c3b5
paisap <santiagoaldana@gmail.com>
2021-01-01 17:53:07 -0500

    private atribute

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -12,7 +12,7 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.W = np.random.randn(1, nx)
+        self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
 
