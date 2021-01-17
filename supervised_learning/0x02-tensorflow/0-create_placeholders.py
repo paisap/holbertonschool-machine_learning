756f9ec290010a141863a7e8c2d665a0e92d3f0e
paisap <santiagoaldana58@gmail.com>
2021-01-17 06:37:10 -0500

    ya tengo tensorflow

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -5,6 +5,6 @@ import tensorflow as tf
 
 def create_placeholders(nx, classes):
     """ create two place"""
-    x = tf.placeholder(dtype=float32, shape=(None, nx), name="x")
-    y = tf.placeholder(dtype=float32, shape=(None, classes), name="y")
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
+    y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
     return x, y
