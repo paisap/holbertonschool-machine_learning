1ab59ec328c8f336d34d4fb5d12bfca5f73ef503
paisap <santiagoaldana58@gmail.com>
2021-01-26 10:19:28 -0500

    documentation

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -4,7 +4,7 @@ import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    
+    """ that builds a neural network with the Keras library: """
     NN = K.Sequential()
     regs = K.regularizers.L1L2(l2=lambtha)
     NN.add(K.layers.Dense(units=layers[0], activation=activations[0],
