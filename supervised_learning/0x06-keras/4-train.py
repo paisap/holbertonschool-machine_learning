a2e5ff8d7c95ef74ea9f8fea467cbbcf5fdc9842
paisap <santiagoaldana58@gmail.com>
2021-01-26 13:17:09 -0500

    task 4

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -6,5 +6,6 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size,
                 epochs, verbose=True, shuffle=False):
     """ that trains a model using mini-batch gradient descent: """
-    return network.fit(data, labels, batch_size=batch_size, epochs=epochs,
-                       verbose=verbose, shuffle=shuffle)
+    history = network.fit(data, labels, batch_size=batch_size, epochs=epochs,
+                          verbose=verbose, shuffle=shuffle)
+    return history
