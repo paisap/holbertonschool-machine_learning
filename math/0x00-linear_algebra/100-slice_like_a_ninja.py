218db47b73fd49e2b588bb5dce187bdd57b81a34
paisap <santiagoaldana58@gmail.com>
2021-04-14 03:26:59 -0500

    primer avanzado

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" that slices a matrix along specific axes """
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    """ that slices a matrix along specific axes """
+    empty_slicer = slice(None, None, None)
+    slice_list = [empty_slicer] * len(matrix.shape)
+
+    for key, value in sorted(axes.items()):
+        slice_list[key] = slice(*value)
+    x = tuple(slice_list)
+    return matrix[x]
