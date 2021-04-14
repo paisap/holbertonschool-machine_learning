e23ad6a5ff0261163f4364d31d59bf1e5e3be20c
paisap <santiagoaldana58@gmail.com>
2021-04-14 01:39:04 -0500

    pep8

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -4,5 +4,7 @@
 
 def matrix_transpose(matrix):
     """ that returns the transpose of a 2D matrix """
-    new_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    l1 = len(matrix)
+    l2 = len(matrix[0])
+    new_matrix = [[matrix[j][i] for j in range(l1)] for i in range(l2))]
     return new_matrix
