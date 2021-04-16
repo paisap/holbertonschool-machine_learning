68694fbc387aa1d72d63e8c4a2217954c7892171
Santiago Aldana <55111413+paisap@users.noreply.github.com>
2021-04-15 22:32:00 -0500

    Update 7-gettin_cozy.py

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -3,12 +3,6 @@
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
-    mat = []
-    size = len(mat1) + len(mat2)
-    for i in range(size):
-        aux = mat1[i] + mat2[i]
-        mat.append(aux)
-    return mat
     """ concatanet """
     new_mat = []
     if axis == 0:
